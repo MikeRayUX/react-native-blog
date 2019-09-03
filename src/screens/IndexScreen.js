c6eb62cd267c95874c899e31daa13d9926f95123
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-03 15:04:46 -0700

    Reusable provider

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 
 const IndexScreen = () => {
+  const { data, addBlogPost} = useContext(BlogContext);
   return (
     <View>
-      <Text>Index screen</Text>
+      <Button 
+        title="Add Post"
+        onPress={() => addBlogPost()}
+      />
+      <FlatList 
+        data={data}
+        keyExtractor={(post) => post.title }
+        renderItem={({ item }) => {
+          return <Text>
+            {item.title}
+          </Text>
+        }}
+      />
+
+      
     </View>
   )
 }
