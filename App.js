831a9f1e3584674d3b7f6c5b0a661b5199fa8fac
michaelarriaga <m.arriaga.smb@gmail.com>
2019-09-02 20:04:44 -0700

    asopdifj

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 
-const navigator = createStackNavigator({
-  Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: "Blogs"
-  }
-});
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: "Blogs"
+    }
+  });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />
+}
